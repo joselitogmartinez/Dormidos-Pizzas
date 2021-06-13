01b9c3521f384765c98fde68e829605220941135
JoselitGiron <joselitogiron.a@gmail.com>
2021-06-13 17:44:59 -0600

    Subiendo los archivos del programa

---
+++ src/main/java/Administrador/PizzaJal.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Administrador;
+
+/**
+ *
+ * @author PC
+ */
+public class PizzaJal {
+    
+    String Nombre,Descripcion,Precio;
+
+    public PizzaJal(String Nombre, String Descripcion, String Precio) {
+        this.Nombre = Nombre;
+        this.Descripcion = Descripcion;
+        this.Precio = Precio;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public String getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(String Precio) {
+        this.Precio = Precio;
+    }
+    
+}
---
+++ src/main/java/Administrador/PizzaPG.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Administrador;
+
+/**
+ *
+ * @author PC
+ */
+public class PizzaPG {
+    
+    String Nombre,Descripcion,Precio;
+
+    public PizzaPG(String Nombre, String Descripcion, String Precio) {
+        this.Nombre = Nombre;
+        this.Descripcion = Descripcion;
+        this.Precio = Precio;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public String getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(String Precio) {
+        this.Precio = Precio;
+    }
+    
+    
+}
